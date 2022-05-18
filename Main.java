c9412563ff95f080901ed2e082fd8e2c33a82bf3
Murali <murali.151298@gmail.com>
2022-05-18 01:32:40 +0000

    first java code

---
+++ Main.java
@@ -0,0 +1,15 @@
+package mkp;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int number1 = 5;
+		int number2 = 6;
+		int result = number1 + number2;
+		
+		
+		System.out.println("hello world " + result);
+
+	}
+
+}
